3027c43a6100ecb7cc440006046915a795a26adf
Sagar Shah <sagarshah2326@gmail.com>
2018-09-05 17:49:44 +0000

    msm8937-common: Don't copy aanc_tuning_mixer.txt
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -106,7 +106,6 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio-caf/msm8996/configs/msm8937/aanc_tuning_mixer.txt:system/vendor/etc/aanc_tuning_mixer.txt \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     hardware/qcom/audio-caf/msm8996/configs/msm8937/audio_platform_info_extcodec.xml:system/vendor/etc/audio_platform_info_extcodec.xml \
