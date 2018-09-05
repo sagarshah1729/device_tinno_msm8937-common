0e14c5e2fe6663a1d0b9bd051cfa6fb1d7995918
Sagar Shah <sagarshah2326@gmail.com>
2018-09-05 17:30:16 +0000

    msm8937-common: Remove duplicate filesystem flag
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,9 +146,6 @@ USE_OPENGL_RENDERER := true
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(COMMON_PATH)/android_filesystem_config.h
-
 # FM
 BOARD_HAVE_QCOM_FM := true
 TARGET_QCOM_NO_FM_FIRMWARE := true
