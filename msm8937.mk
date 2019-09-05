4b2048861cb001209f51719d516de7f299045b21
Sagar Shah <sagarshah2326@gmail.com>
2019-09-05 15:23:46 +0200

    Revert "msm8937-common: Migrate to livedisplay 2.0"
    This reverts commit c58a07114758fcb8b5f2f839a12dcc69f3fc5f61.

--- msm8937.mk
+++ msm8937.mk
@@ -249,9 +249,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8937
 
-# LiveDisplay
+# LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
